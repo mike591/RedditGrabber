8cca7cdb529e02302f3fa13a7b82beb48af0dff5
Michael Mach <mikemach123@gmail.com>
2016-03-14 13:00:30 -0700

    Created Recycler View and Fragment

---
+++ app/src/main/java/com/michaelallan/android/redditgrabber/RedditGrabberFragment.java
@@ -0,0 +1,46 @@
+package com.michaelallan.android.redditgrabber;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by Myko on 3/14/2016.
+ */
+public class RedditGrabberFragment extends Fragment{
+
+    public static RedditGrabberFragment newInstance() {
+        return new RedditGrabberFragment();
+    }
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+
+    public class RedditGrabberViewHolder extends RecyclerView.ViewHolder {
+        public RedditGrabberViewHolder(View itemView) {
+            super(itemView);
+        }
+    }
+
+    public class RedditGrabberAdapter extends RecyclerView.Adapter {
+        @Override
+        public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            return null;
+        }
+
+        @Override
+        public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+
+        }
+
+        @Override
+        public int getItemCount() {
+            return 0;
+        }
+    }
+
+}
