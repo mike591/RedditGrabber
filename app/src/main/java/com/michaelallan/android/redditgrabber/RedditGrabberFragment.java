93685b2615774435f9ea9e1f849f4980cb82e587
Michael Mach <mikemach123@gmail.com>
2016-03-14 13:28:59 -0700

    Now uses support compat fragment

--- app/src/main/java/com/michaelallan/android/redditgrabber/RedditGrabberFragment.java
+++ app/src/main/java/com/michaelallan/android/redditgrabber/RedditGrabberFragment.java
@@ -1,7 +1,8 @@
 package com.michaelallan.android.redditgrabber;
 
-import android.app.Fragment;
+
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.view.ViewGroup;
@@ -9,7 +10,7 @@ import android.view.ViewGroup;
 /**
  * Created by Myko on 3/14/2016.
  */
-public class RedditGrabberFragment extends Fragment{
+public class RedditGrabberFragment extends Fragment {
 
     public static RedditGrabberFragment newInstance() {
         return new RedditGrabberFragment();
